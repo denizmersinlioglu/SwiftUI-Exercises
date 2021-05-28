96abda835481bec3d2c4eaea6430ffb7d07fb489
Deniz <deniz@socio.events>
2021-05-29 01:48:46 +0300

    Initial Commit

---
+++ SwiftUI-Exercises/SwiftUI_ExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExercisesApp.swift
+//  SwiftUI-Exercises
+//
+//  Created by Deniz Mersinlioğlu on 29.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
